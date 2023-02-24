268d6b11bca314a1e96fde924a3507053d4724ae
giovannitamburini <giovatambu@gmail.com>
2023-02-24 14:42:16 +0100

    added pseudo code

--- js/script.js
+++ js/script.js
@@ -12,3 +12,29 @@ Scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
 Proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro
 programma così come lo faremmo "a mano"
 */
+
+// PSEUDO CODICE
+
+/*
+-creo nell'HTML un elemento "ul" con un id, dentro il quale ci appenderò ciò che
+ viene prodotto all'interno del ciclo che andrò a creare
+
+-dichiarare le variabili necessarie
+
+-creo un ciclo con variabile con una variabile che si ripete finchè la variabile
+ indicata non eguaglia il limite che gli abbiamo imposto (=<100).
+
+-all'interno del ciclo pongo diverse condizioni:
+?SE la variabile è divisibile per 3 o per 5:
+
+  ?SE la variabile è divisibile sia per 3 che per 5:
+  °stampa in console "FizzBuzz";
+
+  ?ALTRIMENTI SE la variabile è divisibile solo per 3:
+  °stampa in console "Fizz";
+
+  ?ALTRIMENTI (la variabile è divisibile solo per 5):
+  stampa in console "Buzz".
+
+?ALTRIMENTI:
+°stampa il valore della variabile
